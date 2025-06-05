f0f297a10127955a48255e6b46d8c91fc22d80aa
niran <nirancz990@gmail.com>
2025-06-05 11:09:40 +0300

    Reservation now implements Serializable

--- bpark/src/logic/Reservation.java
+++ bpark/src/logic/Reservation.java
@@ -1,21 +1,25 @@
 package logic;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 /**
  * Simple POJO to hold one reservation record.
  */
-public class Reservation {
-    private final String    spot;
+public class Reservation implements Serializable{
+    
+	private final String    spot;
+    private final int    subscriberId;
     private final LocalDate date;
     private final String    startTime;
     private final String    endTime;
 
-    public Reservation(String spot, LocalDate date, String startTime, String endTime) {
+    public Reservation(String spot,int subscriberId, LocalDate date, String startTime, String endTime) {
         this.spot      = spot;
         this.date      = date;
         this.startTime = startTime;
         this.endTime   = endTime;
+        this.subscriberId=subscriberId;
     }
 
     public String getSpot() {
@@ -33,5 +37,9 @@ public class Reservation {
     public String getEndTime() {
         return endTime;
     }
+
+	public int getSubscriberId() {
+		return subscriberId;
+	}
 }
 
